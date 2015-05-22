de1c887de93cfcccf1334450e76244f1cfb43f2e
Henna Ruohonen <varpushaukka@oiva.sange.fi>
2015-05-22 12:35:13 +0300

    path korjattu

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -2,6 +2,6 @@ from bottle import route, run
 
 @route('/static/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='/proj/pistedatatallennin/src/sivut')
+    return static_file(filepath, root='/home/varpushaukka/proj/pistedatatallennin/src/sivut')
 
 run(host='0.0.0.0', port=8088, debug=True)
