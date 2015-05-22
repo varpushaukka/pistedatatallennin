4db3b2545ac721af3028e18cf07cd9d6a12ef708
Henna Ruohonen <varpushaukka@oiva.sange.fi>
2015-05-22 12:31:49 +0300

    korjaus pistesovellukseen

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -1,12 +1,7 @@
 from bottle import route, run
 
-@route('/sivut/index.html')
-
-#def server_static(index.html):
-#    return static_file(index.html, root='/home/varpushaukka/yliopisto/tsoha/pistedatatallennin/src/sivut')
-
 @route('/static/<filepath:path>')
 def server_static(filepath):
-    return static_file(filepath, root='/sivut/')
+    return static_file(filepath, root='/proj/pistedatatallennin/src/sivut')
 
 run(host='0.0.0.0', port=8088, debug=True)
