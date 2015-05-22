ea5a10deefa4128f78b8796077d392ff9ef60d77
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2015-05-22 12:53:19 +0300

    status lol unohtu

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -3,7 +3,7 @@ from bottle import route, run
 @route('/static/<filepath:path>')
 def server_static(filepath):
     f = static_file(filepath, root='/home/varpushaukka/proj/pistedatatallennin/src/sivut')
-    print f
+    print f.status
     return f
 
 run(host='0.0.0.0', port=8088, debug=True)
