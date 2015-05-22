d5b0d48e7b20fe4d5b41337297be800eb16343ef
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2015-05-22 12:12:57 +0300

    yritys saada sivu näkymään1

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -2,7 +2,11 @@ from bottle import route, run
 
 @route('/sivut/index.html')
 
-def server_static(index.html):
-    return static_file(index.html, root='/home/varpushaukka/yliopisto/tsoha/pistedatatallennin/src/sivut')
+#def server_static(index.html):
+#    return static_file(index.html, root='/home/varpushaukka/yliopisto/tsoha/pistedatatallennin/src/sivut')
+
+@route('/static/<filepath:/sivut/>')
+def server_static(filepath):
+    return static_file(filepath, root='/sivut/')
 
 run(host='0.0.0.0', port=8088, debug=True)
