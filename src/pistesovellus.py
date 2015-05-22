05bc0e929dcf4ccecc36f91d4e1156dc3eec3a39
Henna Ruohonen <varpushaukka@oiva.sange.fi>
2015-05-22 12:14:35 +0300

    joo

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -2,7 +2,7 @@ from bottle import route, run
 
 @route('/sivut/index.html')
 
-def server_static(index.html):
-    return static_file(index.html, root='/home/varpushaukka/yliopisto/tsoha/pistedatatallennin/src/sivut')
+def server_static(index):
+    return static_file(index, root='/home/varpushaukka/yliopisto/tsoha/pistedatatallennin/src/sivut')
 
 run(host='0.0.0.0', port=8088, debug=True)
