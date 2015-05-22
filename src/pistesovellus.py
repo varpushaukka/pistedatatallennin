f6b96fa815fe7786febcb92ae92cb09543fdf0d6
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2015-05-22 13:03:17 +0300

    debugprintti poistettu

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -3,7 +3,6 @@ from bottle import route, run, static_file
 @route('/sivut/<filepath>')
 def server_static(filepath):
     f = static_file(filepath, root='/home/varpushaukka/proj/pistedatatallennin/src/sivut')
-    print f.status
     return f
 
 run(host='0.0.0.0', port=8088, debug=True)
