f3e6d29faf5899bbb287072b1b4e6015d3c1e600
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2015-05-27 13:30:57 +0300

    korjattu createtables ja lisätty dbconnect sovellukseen

--- src/pistesovellus.py
+++ src/pistesovellus.py
@@ -1,13 +1,27 @@
 from bottle import route, run, static_file, redirect
 from os.path import realpath, dirname
+from psycopg2 import connect
 
 script_dir = dirname(realpath(__file__))
+
+cur = connect("dbname=pistedata").cursor()
+
+"olen omena"
 
 @route('/sivut/<filepath>')
 def server_static(filepath):
     return static_file(filepath, root=script_dir + '/sivut')
 
++358-400-769677 ; "soita Suskille" #kommentti?
+
 @route('/')
 def default_page(): redirect('sivut/index.html')
 
+def sql(query):
+   cur.execute(query)
+   cur.fetchall()
+
+
 run(host='0.0.0.0', port=8088, debug=True)
+
+   
